13f982a7109fd3f1d175ec788d1ce48b7e9fc1cb
gabrielgiroe1 <gabriel.giroe@gmail.com>
2022-06-21 15:33:26 +0300

    wdk

--- prototype/prototype.rb
+++ prototype/prototype.rb
@@ -1,5 +1,3 @@
-# The example class that has cloning ability. We'll see how the values of field
-# with different types will be cloned.
 class Prototype
   attr_accessor :primitive, :component, :circular_reference
 
@@ -13,16 +11,11 @@ class Prototype
   def clone
     @component = deep_copy(@component)
 
-    # Cloning an object that has a nested object with backreference requires
-    # special treatment. After the cloning is completed, the nested object
-    # should point to the cloned object, instead of the original object.
     @circular_reference = deep_copy(@circular_reference)
     @circular_reference.prototype = self
     deep_copy(self)
   end
 
-  # deep_copy is the usual Marshalling hack to make a deep copy. But it's rather
-  # slow and inefficient, therefore, in real applications, use a special gem
   private def deep_copy(object)
     Marshal.load(Marshal.dump(object))
   end
